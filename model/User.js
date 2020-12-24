9ced40737026d56afc6ac164b03bd49749962b4e
Mouad <devmahfoud@gmail.com>
2020-12-24 20:47:29 +0100

    adding some roles to user

--- model/User.js
+++ model/User.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         required: true,
-        enum: ['admin', 'cooker', 'user']
+        enum: ['admin', 'cooker', 'user', "restaurant", "bakery"]
     },
     tokens: [{
         token: {
